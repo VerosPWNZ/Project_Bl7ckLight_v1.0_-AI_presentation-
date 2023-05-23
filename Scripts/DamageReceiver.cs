57366f6db3fffbe90c0caa5858deea5eded12f3c
VerosPWNZ <vlad_cool_24@mail.ru>
2023-05-23 20:14:46 +0300

    AI code
    This folder will show, what code i used to create AI im my project

---
+++ Scripts/DamageReceiver.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class DamageReceiver : MonoBehaviour, IEntity
+{
+    //This script will keep track of player HP
+    public float playerHP = 100;
+    public CharacterController playerController;
+    public WeaponManager weaponManager;
+
+    public void ApplyDamage(float points)
+    {
+        playerHP -= points;
+
+        if(playerHP <= 0)
+        {
+            //Player is dead
+            playerController.canMove = false;
+            playerHP = 0;
+        }
+    }
+}
